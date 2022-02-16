1c6747900c9aaf5a7a89d946acc07d31764823e3
NatachaBelboom <natacha.belboom@student.hepl.be>
2022-02-16 15:21:51 +0100

    Added trips custom post type

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -5,3 +5,16 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 // Activer les images sur les articles
 add_theme_support('post-thumbnails');
+
+// Enregistrer un "type de ressource" (custom post type) pour les voyage
+register_post_type('trips', [
+   'label' => 'Voyages',
+   'labels' => [ //Ecraser des valeurs par defaut
+       'name' => 'Voyages',
+       'singular_name' => 'Voyage',
+   ],
+    'description' => "La ressource permettant de gérer les voyages qui ont été effectués",
+    'public' => true, //accessible dans l'interface admin (formulaire de contact: false)
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-palmtree',
+]);
